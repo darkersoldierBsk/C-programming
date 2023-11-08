0f25c2f0d2afe02b677d2bf3f5ac67f0e9ca9d36
darkersoldierBsk <134071847+darkersoldierBsk@users.noreply.github.com>
2023-11-08 23:14:16 +0300

    Add files via upload
    infinite loops

---
+++ infinite_loops.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    // LOOP 1  //
+    
+    for(int a = 1; a <= 10; a/= 2){
+        printf("%d\n", a);
+    }
+
+
+    // lOOP 2 //
+    
+    for(; ;){  
+        printf("for infinityy");
+    }  
+    return 0;
+    
+    
+    // LOOP 3 //
+    
+    while(1){  
+        printf("while infinityy");
+    }
+    
+    
+    // lOOP 4 //
+    
+    do
+    {
+        printf("do while infinityy");
+    }while(1);
+    
+    
+}
+
