048963171cc200bfd234b451ce997afe933691ce
darkersoldierBsk <134071847+darkersoldierBsk@users.noreply.github.com>
2023-11-09 01:46:17 +0300

    Add files via upload
    Try to make a chessboard like this
    
    Take a number to make a chessboard: (ex) 3
    
    * *
     *           3x3 chessboard (* = black ones, "(space)" = white ones )
    * *

---
+++ chessboard_asterix.c
@@ -0,0 +1,27 @@
+/* Try a chessboard like this
+
+Take a number to make a chessboard: (ex) 3
+
+* *
+ *           3x3 chessboard (* = black ones, "(space)" = white ones )
+* *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter a number to make a chess playground: ");
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; i++){
+        for(int j = 1; j <= num; j++){
+            if( (i+j) % 2 == 0) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
